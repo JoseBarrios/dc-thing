9b105d5dc652fee29996fa892c164a94f8f06175
Jose Barrios <jose@cognilab.com>
2017-06-19 22:27:16 -0700

    Added validation methods and unit tests for everything

--- index.js
+++ index.js
@@ -1,6 +1,75 @@
+'use strict'
+
+const assert = require('assert');
+const _ = require('lodash');
 const EMPTY_STRING = '';
 
 class Thing {
+
+  /////////////////////
+  //
+  // Static Methods
+  //
+  /////////////////////
+  static logError(error){ console.error(error); }
+  static isNumber(value){ return _.isNumber(value); }
+  static isString(value){ return _.isString(value); }
+  static isBoolean(value){ return _.isBoolean(value); }
+  static isArray(value){ return _.isArray(value); }
+  static isObject(value){ return _.isPlainObject(value); }
+  static isNull(value){ return _.isNull(value); }
+  static isUndefined(value){ return _.isUndefined(value); }
+
+  static isEmpty(value){
+    return Thing.isUndefined(value) || Thing.isNull(value) || value === '';
+  }
+
+  static isURL(value){
+    let isString = Thing.isString(value);
+    let URL = new RegExp(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi);
+    return  isString && URL.test(value);
+  }
+
+  static validateInputType(type, value, propName){
+    var error = new TypeError(`${propName} must be a ${type}`);
+    switch(type.toLowerCase()){
+      case 'number':
+        if(!Thing.isNumber(value) && !Thing.isEmpty(value)) throw error;
+        break;
+      case 'string':
+        var stringError = new TypeError(`${propName} must be a ${type}`);
+        if(!Thing.isString(value) && !Thing.isEmpty(value)) throw error;
+        break;
+      case 'boolean':
+        var booleanError = new TypeError(`${propName} must be a ${type}`);
+        if(!Thing.isBoolean(value) && !Thing.isEmpty(value)) throw error;
+        break;
+      case 'array':
+        var arrayError = new TypeError(`${propName} must be a ${type}`);
+        if(!Thing.isArray(value) && !Type.isEmpty(value)) throw error;
+        break;
+      case 'object':
+        var objectError = new TypeError(`${propName} must be a ${type}`);
+        if(!Thing.isObject(value) && !Type.isEmpty(value)) throw error;
+        break;
+      case 'null':
+        var nullError = new TypeError(`${propName} must be a ${type}`);
+        if(!Thing.isNull(value) && !Type.isEmpty(value)) throw error;
+        break;
+      default:
+        var defaultError = new TypeError(`${propName} must be a ${type}`);
+        throw defaultError;
+    }
+  }
+
+  static isValidInput(value){
+    return Thing.isNumber(value) ||
+      Thing.isString(value) ||
+      Thing.isBoolean(value) ||
+      Thing.isArray(value) ||
+      Thing.isObject(value) ||
+      Thing.isNull(value);
+  }
 
   constructor(model){
     model = model || {};
@@ -21,63 +90,100 @@ class Thing {
     this.potentialAction = model.potentialAction;
     this.sameAs = model.sameAs;
     this.url = model.url;
-
   }
+
 
   get additionalType(){ return this.computed.additionalType; }
   set additionalType(value){
-    this.computed.additionalType = value;
+    try {
+      Thing.validateInputType('string', value, 'additionalType');
+      if(Thing.isURL(value)){ this.computed.additionalType = value; }
+    } catch(error){ Thing.logError(error); }
   }
 
   get alternateName(){ return this.computed.alternateName; }
   set alternateName(value){
-    this.computed.alternateName = value;
+    try {
+      Thing.validateInputType('string', value, 'alternateName');
+      this.computed.alternateName = value;
+    } catch(error){ Thing.logError(error); }
   }
 
   get description(){ return this.computed.description; }
   set description(value){
-    this.computed.description = value;
+    try {
+      Thing.validateInputType('string', value, 'description');
+      this.computed.description = value;
+    } catch(error){ Thing.logError(error); }
   }
 
   get disambiguatingDescription(){ return this.computed.disambiguatingDescription; }
   set disambiguatingDescription(value){
-    this.computed.disambiguatingDescription = value;
+    try {
+      Thing.validateInputType('string', value, 'disambiguatingDescription');
+      this.computed.disambiguatingDescription = value;
+    } catch(error){ Thing.logError(error); }
   }
 
   get identifier(){ return this.computed.identifier; }
   set identifier(value){
-    this.computed.identifier = value;
+    try {
+      Thing.validateInputType('string', value, 'identifier');
+      this.computed.identifier = value;
+    } catch(error){ Thing.logError(error); }
   }
 
   get image(){ return this.computed.image; }
   set image(value){
-    this.computed.image = value;
+    try {
+      Thing.validateInputType('string', value, 'image');
+      if(Thing.isURL(value)){ this.computed.image = value; }
+    } catch(error){ Thing.logError(error); }
   }
 
-  get mainEntityOfPage(){ return this.computed.mainEntityOfPage; }
-  set mainEntityOfPage(value){
-    this.computed.mainEntityOfPage = value;
-  }
+  //TODO
+  //get mainEntityOfPage(){ return this.computed.mainEntityOfPage; }
+  //set mainEntityOfPage(value){
+    //try {
+      //Thing.validateInputType('string', value, 'mainEntityOfPage');
+      //this.computed.mainEntityOfPage = value;
+    //} catch(error){ Thing.logError(error); }
+  //}
 
   get name(){ return this.computed.name; }
   set name(value){
-    this.computed.name = value;
+    try {
+      Thing.validateInputType('string', value, 'name');
+      this.computed.name = value;
+    } catch(error){ Thing.logError(error); }
   }
 
-  get potentialAction(){ return this.computed.potentialAction; }
-  set potentialAction(value){
-    this.computed.potentialAction = value;
-  }
+  //TODO
+  //get potentialAction(){ return this.computed.potentialAction; }
+  //set potentialAction(value){
+    //try {
+      //Thing.validateInputType('string', value, 'potentialAction');
+      //this.computed.potentialAction = value;
+    //} catch(error){ Thing.logError(error); }
+  //}
 
   get sameAs(){ return this.computed.sameAs; }
   set sameAs(value){
-    this.computed.sameAs = value;
+    try {
+      Thing.validateInputType('string', value, 'sameAs');
+      if(Thing.isURL(value)){ this.computed.sameAs = value; }
+    } catch(error){ Thing.logError(error); }
   }
 
   get url(){ return this.computed.url; }
   set url(value){
-    this.computed.url = value;
+    try {
+      Thing.validateInputType('string', value, 'url');
+      if(Thing.isURL(value)){ this.computed.url = value; }
+    } catch(error){ Thing.logError(error); }
   }
+
+
 
   /////////////////////
   //
@@ -87,12 +193,12 @@ class Thing {
   get model(){ return this.computed; }
   set model(value){ this._model = this.computed; }
 
+
   set emptyProperties(value){ this._emptyProperties = this.computed; }
   get emptyProperties(){
     let emptyProperties = {};
-    Object.keys(this.computed).forEach(key =>{
-      let value = this.computed[key];
-      if(!value || value === EMPTY_STRING){
+    _.forIn(this.computed, (value, key) => {
+      if(Thing.isEmpty(value)){
         emptyProperties[key] = value;
       }
     })
--- tests/test.js
+++ tests/test.js
@@ -11,14 +11,219 @@ model.alternateName = undefined;
 model.description = null;
 let thing = new Thing(model);
 
-describe('thing.model', function() {
-	it('should return the serialized version of the model', function() {
-    assert.equal(thing.model.name, 'Jose Barrios');
-	});
-});
+describe('Thing Class\n', function() {
 
-describe('thing.emptyProperties', function() {
-	it('should return a new object with all the empty properties of this', function() {
-    assert.equal(thing.emptyProperties.additionalType, '');
-	});
+  describe('Static Methods:', function() {
+    describe('Thing.isNumber(value)', function() {
+      it('should return true if value is a number, false otherwise', function() {
+        assert.equal(Thing.isNumber(1), true);
+        assert.equal(Thing.isNumber('string'), false);
+        assert.equal(Thing.isNumber(''), false);
+        assert.equal(Thing.isNumber(true), false);
+        assert.equal(Thing.isNumber(false), false);
+        assert.equal(Thing.isNumber([]), false);
+        assert.equal(Thing.isNumber({}), false);
+        assert.equal(Thing.isNumber(null), false);
+        assert.equal(Thing.isNull(undefined), false);
+      });
+    });
+    describe('Thing.isString(value)', function() {
+      it('should return true if value is string, false otherwise', function() {
+        assert.equal(Thing.isString(1), false);
+        assert.equal(Thing.isString('string'), true);
+        assert.equal(Thing.isString(''), true);
+        assert.equal(Thing.isString(true), false);
+        assert.equal(Thing.isString(false), false);
+        assert.equal(Thing.isString([]), false);
+        assert.equal(Thing.isString({}), false);
+        assert.equal(Thing.isString(null), false);
+        assert.equal(Thing.isNull(undefined), false);
+      });
+    });
+    describe('Thing.isBoolean(value)', function() {
+      it('should return true if value is boolean, false otherwise', function() {
+        assert.equal(Thing.isBoolean(1), false);
+        assert.equal(Thing.isBoolean('string'), false);
+        assert.equal(Thing.isBoolean(''), false);
+        assert.equal(Thing.isBoolean(true), true);
+        assert.equal(Thing.isBoolean(false), true);
+        assert.equal(Thing.isBoolean([]), false);
+        assert.equal(Thing.isBoolean({}), false);
+        assert.equal(Thing.isBoolean(null), false);
+        assert.equal(Thing.isNull(undefined), false);
+      });
+    });
+    describe('Thing.isArray(value)', function() {
+      it('should return true if value is array, false otherwise', function() {
+        assert.equal(Thing.isArray(1), false);
+        assert.equal(Thing.isArray('string'), false);
+        assert.equal(Thing.isArray(''), false);
+        assert.equal(Thing.isArray(true), false);
+        assert.equal(Thing.isArray(false), false);
+        assert.equal(Thing.isArray([]), true);
+        assert.equal(Thing.isArray({}), false);
+        assert.equal(Thing.isArray(null), false);
+        assert.equal(Thing.isNull(undefined), false);
+      });
+    });
+    describe('Thing.isObject(value)', function() {
+      it('should return true if value is plain object, false otherwise', function() {
+        assert.equal(Thing.isObject(1), false);
+        assert.equal(Thing.isObject('string'), false);
+        assert.equal(Thing.isObject(''), false);
+        assert.equal(Thing.isObject(true), false);
+        assert.equal(Thing.isObject(false), false);
+        assert.equal(Thing.isObject([]), false);
+        assert.equal(Thing.isObject({}), true);
+        assert.equal(Thing.isObject({ "hello" : "world"}), true);
+        assert.equal(Thing.isObject({ hello : "world"}), true);
+        assert.equal(Thing.isObject({ greeting : {hello: "world"}}), true);
+        assert.equal(Thing.isObject(null), false);
+        assert.equal(Thing.isNull(undefined), false);
+      });
+    });
+    describe('Thing.isNull(value)', function() {
+      it('should return true if value is null, false otherwise', function() {
+        assert.equal(Thing.isNull(1), false);
+        assert.equal(Thing.isNull('string'), false);
+        assert.equal(Thing.isNull(''), false);
+        assert.equal(Thing.isNull(true), false);
+        assert.equal(Thing.isNull(false), false);
+        assert.equal(Thing.isNull([]), false);
+        assert.equal(Thing.isNull({}), false);
+        assert.equal(Thing.isNull(null), true);
+        assert.equal(Thing.isNull(undefined), false);
+      });
+    });
+    describe('Thing.isValidInput(value)', function() {
+      it('should return true if value is JSON supported, false otherwise', function() {
+        assert.equal(Thing.isValidInput(1), true);
+        assert.equal(Thing.isValidInput('string'), true);
+        assert.equal(Thing.isValidInput(''), true);
+        assert.equal(Thing.isValidInput(true), true);
+        assert.equal(Thing.isValidInput(true), true);
+        assert.equal(Thing.isValidInput([]), true);
+        assert.equal(Thing.isValidInput({}), true);
+        assert.equal(Thing.isValidInput(null), true);
+        assert.equal(Thing.isValidInput(undefined), false);
+      });
+    });
+    describe('Thing.isURL(value)', function() {
+      it('should return true if value is a valid URL, false otherwise\n', function() {
+        assert.equal(Thing.isURL('www.google.com'), true);
+        assert.equal(Thing.isURL('google.com'), true);
+        assert.equal(Thing.isURL('http://google.com'), true);
+        assert.equal(Thing.isURL('https://google.com'), true);
+        assert.equal(Thing.isURL('https://google.com/something'), true);
+        assert.equal(Thing.isURL('https://google.com/something?value=true'), true);
+        assert.equal(Thing.isURL(1), false);
+        assert.equal(Thing.isURL('string'), false);
+        assert.equal(Thing.isURL(''), false);
+        assert.equal(Thing.isURL(true), false);
+        assert.equal(Thing.isURL(true), false);
+        assert.equal(Thing.isURL([]), false);
+        assert.equal(Thing.isURL({}), false);
+        assert.equal(Thing.isURL(null), false);
+        assert.equal(Thing.isURL(undefined), false);
+      });
+    });
+  });
+
+
+  describe('Properties:', function() {
+    describe('thing.additionalType', function() {
+      it('should only allow value to be set if it is of type string (url)', function() {
+        let url = 'google.com';
+        thing.additionalType = url;
+        assert.equal(thing.additionalType, url);
+      });
+    });
+    describe('thing.alternateName', function() {
+      it('should only allow value to be set if it is of type string', function() {
+        let str = 'alternateName';
+        thing.alternateName = str;
+        assert.equal(thing.alternateName, str);
+      });
+    });
+    describe('thing.description', function() {
+      it('should only allow value to be set if it is of type string', function() {
+        let str = 'description';
+        thing.description = str;
+        assert.equal(thing.description, str);
+      });
+    });
+    describe('thing.disambiguatingDescription', function() {
+      it('should only allow value to be set if it is of type string', function() {
+        let str = 'disambiguatingDescription';
+        thing.disambiguatingDescription = str;
+        assert.equal(thing.disambiguatingDescription, str);
+      });
+    });
+    describe('thing.identifier', function() {
+      it('should only allow value to be set if it is of type string', function() {
+        let str = 'identifier';
+        thing.identifier = str;
+        assert.equal(thing.identifier, str);
+      });
+    });
+    describe('thing.image', function() {
+      it('should only allow value to be set if it is of type string (url)', function() {
+        let url = 'google.com';
+        thing.image = url;
+        assert.equal(thing.image, url);
+      });
+    });
+    //TODO
+    //describe('thing.mainEntityOfPage', function() {
+    //it('should only allow value to be set if it is of type string', function() {
+    //let str = 'mainEntityOfPage';
+    //thing.mainEntityOfPage = str;
+    //assert.equal(thing.mainEntityOfPage, str);
+    //});
+    //});
+    describe('thing.name', function() {
+      it('should only allow value to be set if it is of type string', function() {
+        let str = 'name';
+        thing.name = str;
+        assert.equal(thing.name, str);
+      });
+    });
+    //TODO
+    //describe('thing.potentialAction', function() {
+    //it('should only allow value to be set if it is of type string', function() {
+    //let str = 'potentialAction';
+    //thing.potentialAction = str;
+    //assert.equal(thing.potentialAction, str);
+    //});
+    //});
+    describe('thing.sameAs', function() {
+      it('should only allow value to be set if it is of type string (url)', function() {
+        let url = 'google.com';
+        thing.sameAs = url;
+        assert.equal(thing.sameAs, url);
+      });
+    });
+    describe('thing.url', function() {
+      it('should only allow value to be set if it is of type string (url)', function() {
+        let url = 'google.com';
+        thing.url = url;
+        assert.equal(thing.url, url);
+      });
+    });
+    describe('thing.model', function() {
+      it('should return the serialized version of the class (aka model)', function() {
+        assert.equal(thing.model.description, 'description');
+      });
+    });
+    describe('thing.emptyProperties', function() {
+      it('should return the instance empty properties', function() {
+        thing.alternateName = '';
+        thing.description = null;
+        thing.name = undefined;
+        assert.equal(thing.emptyProperties.alternateName, '');
+        assert.equal(thing.emptyProperties.description, null);
+        assert.equal(thing.emptyProperties.name, undefined);
+      });
+    });
+  });
 });
